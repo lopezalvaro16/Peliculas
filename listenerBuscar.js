cdac3ef03f4fb7f58107587e6a276c2ffb2e8a78
lopezalvaro16 <alvaro154020747@gmail.com>
2023-07-27 09:21:31 -0300

    Se agrega los archivos de cargarTitulos.js y listenerBuscar.js

--- listenerBuscar.js
+++
@@ -1,9 +0,0 @@
-import cargarTitulos from "./cargarTitulos";
-import fetchBusqueda from "./fetchBusqueda";
-
-const btn = document.getElementById("btn-buscar");
-btn.addEventListener("click", async (e) => {
-  const resultados = await fetchBusqueda();
-
-  cargarTitulos(resultados);
-});
