6ccaef406e05e93c0da9d9c6701f568a8773c851
lopezalvaro16 <alvaro154020747@gmail.com>
2023-07-28 12:40:10 -0300

    Se agrega los archivos de paginacion.js, fetchItems.js e index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import fetchPopulares from "./fetchPopulares";
+import cargarTitulos from "./cargarTitulos";
+import cargarGeneros from "./cargarGeneros";
+import "./listenerFiltroTipo";
+import "./listenerFiltroGeneros";
+import "./listenerBuscar";
+import "./paginacion";
+import "./listenerItems";
+import "./listenerPopup";
+
+const cargar = async () => {
+  const resultados = await fetchPopulares();
+  cargarTitulos(resultados);
+  cargarGeneros("movie");
+};
+cargar();
