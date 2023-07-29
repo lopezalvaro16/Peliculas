998dc752392a966f9c83d9539eefa7aaf90caa2b
lopezalvaro16 <alvaro154020747@gmail.com>
2023-07-29 14:43:56 -0300

    Se agrega los archivos de listenerPopup.js y listenerItems

--- listenerPopup.js
+++
@@ -1,7 +0,0 @@
-const popup = document.getElementById("media");
-
-popup.addEventListener("click", (e) => {
-  if (e.target.closest("button")) {
-    popup.classList.remove("media--active");
-  }
-});
